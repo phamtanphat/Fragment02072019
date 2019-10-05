22e33d0f564c7892a740bafcc779297657986042
phatdroid94 <phatdroid94@gmail.com>
2019-10-05 19:13:20 +0700

    a

--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/fragment/MainActivity.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/fragment/MainActivity.java
@@ -1,14 +1,22 @@
 package phamtanphat.ptp.khoaphamtraining.fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentManager fragmentManager = getSupportFragmentManager();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        AndroidFragment androidFragment = new AndroidFragment();
+        fragmentTransaction.add(R.id.containerFragment,androidFragment);
+        fragmentTransaction.commit();
     }
 }
