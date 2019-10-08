b2d95d053545f9f3f4610d02141bae5aea10695c
phatdroid94 <phatdroid94@gmail.com>
2019-10-08 17:57:10 +0700

    method get count fragment

--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/fragment/MainActivity.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/fragment/MainActivity.java
@@ -5,14 +5,34 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentManager fragmentManager = getSupportFragmentManager();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+    public void addAndroid(View v){
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        AndroidFragment androidFragment = new AndroidFragment();
+        fragmentTransaction.add(R.id.linearContainer,androidFragment);
+        fragmentTransaction.commit();
 
     }
+    public void addIos(View v){
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        IosFragment iosFragment = new IosFragment();
+        fragmentTransaction.add(R.id.linearContainer,iosFragment);
+        fragmentTransaction.commit();
+
+    }
+//    private void getCountFragmentInActivity(){
+//        Log.d("BBB",fragmentManager.getFragments().size() + "" );
+//    }
 }
